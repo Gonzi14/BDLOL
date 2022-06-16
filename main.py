bd1e6eb2f1e129c5820c1cf5b4a0f230cd7ca9c6
Gonzi14 <gonzaloandrade142@gmail.com>
2022-06-16 18:04:53 -0300

    made it so that it can be updated automaticly after you played a game

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from csv import writer
+from csv import writer, DictWriter
 from filecmp import cmp
 import sys
 from sqlite3 import Row
@@ -12,16 +12,35 @@ import pandas as pd
 from riotwatcher import LolWatcher, ApiError
 import requests
 import json
+# Para ordenar los diccionarios
+import collections
+# Para no matar a la API
+import time
 
 api_key = ''
 watcher = LolWatcher(api_key)
 my_region = 'la2'
-summonerID = "46xpRlJSHOMNuWC9dTkUlSI4idgwCuzi0VJLGxNRnW3U33A"
+me = watcher.summoner.by_name(my_region, 'JimCharles3')
+summonerID = me['id']
+puuid = me['puuid']
 API = (f"https://{my_region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summonerID}" + "?api_key=" + api_key)
+current_champ_list = watcher.data_dragon.champions('12.11.1')
 
+
+def matches(puuid):
+    while True:
+        my_matches = watcher.match.matchlist_by_puuid(my_region, puuid)
+        last_match = my_matches[0]
+        while last_match == my_matches[0]:
+            my_matches = watcher.match.matchlist_by_puuid(my_region, puuid)
+            time.sleep(1)
+            print(last_match)
+        api(API)
+        print("Ya esta")
+        last_match = my_matches[0]
 def bar(dataBase):
     plt.style.use('ggplot')
-    plt.bar(dataBase[0].keys(), dataBase[0].values(), color='#e36685')
+    plt.bar(dataBase.keys(), dataBase.values(), color='#e36685')
     plt.grid()
     plt.show()
 
@@ -33,27 +52,40 @@ def api(API):
         ID = item['championId']
         points = item['championPoints']
         champsDicPoints.update({ID: points})
-    champs_dictName = getAllChampions()
-    for keys in sorted (champsDicPoints) :
-        print((keys, champsDicPoints[keys]), end =" ")
-        if champs_dictName[keys] == champsDicPoints[keys]:
-            champsDicPoints[keys] = {champs_dictName[keys]: points}
-    print(champsDicPoints)
-    #dict_final = {}
+    champsDictName = getAllChampions()
+    pointsDictN = sort(champsDicPoints)
+    nameDictN = sort(champsDictName)
+    for i in nameDictN:
+        try:
+            pointsDictN[nameDictN[i]] = pointsDictN[i]
+            del pointsDictN[i]
+        except:
+            pointsDictN[nameDictN[i]] = 0
+    toCSV(pointsDictN)
+    bar(pointsDictN)    
 
-current_champ_list = watcher.data_dragon.champions('12.11.1')
-#print(current_champ_list)
+def sort(dictionary):
+    items = dictionary.items()
+    itemsSorted = sorted(items)
+    newDict = dict((x, y) for x, y in itemsSorted)
+    return newDict
+
+def toCSV(dict):
+    with open('Campeones.csv', 'w') as f:
+        writer = DictWriter(f, dict.keys())
+        writer.writeheader()
+        writer.writerow(dict)
 
 def getAllChampions():
-    champs_dict = {}
+    champsDict = {}
     file = open('champion.json', encoding= "utf8")
     data2 = json.load(file)
     todo = data2['data']
     for champions in data2['data']:
         champion = todo[champions]
-        id = champion['key']
-        champs_dict.update({id : champions})
-    return champs_dict
+        id = int(champion['key'])
+        champsDict.update({id : champions})
+    return champsDict
 
 def main(tipo, champ, puntos):
     print(tipo)
@@ -91,5 +123,4 @@ def modificar(campeon, puntos):
     df.to_csv(r"Campeones.csv", index = False)
     bar(df['Campeones'], df['Puntos'])
 
-#main(sys.argv[1], sys.argv[2], sys.argv[3])
-api(API)
+matches(puuid)
