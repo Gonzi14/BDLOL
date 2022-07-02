5fb83910f9db609b86c831080af07f958f5a8409
Gonzi14 <gonzaloandrade142@gmail.com>
2022-07-02 00:31:02 -0300

    empiezo con el lado web del proyecto

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from csv import writer, DictWriter
 from filecmp import cmp
 import sys
 from sqlite3 import Row
-from textwrap import dedent
-from xml.etree.ElementTree import tostring
+from tkinter import Menu, ttk
 # Para hacer graficos
 import matplotlib.pyplot as plt
 # Para modificar csv
@@ -12,33 +11,36 @@ import pandas as pd
 from riotwatcher import LolWatcher, ApiError
 import requests
 import json
-# Para ordenar los diccionarios
-import collections
 # Para no matar a la API
 import time
+# Para mandar la informacion a la pagina
+import json
 
-api_key = ''
+# Informacion general de las APIs
+api_key = 'RGAPI-646391cb-df01-494c-aaab-f02654e6a3fd'
 watcher = LolWatcher(api_key)
 my_region = 'la2'
 me = watcher.summoner.by_name(my_region, 'JimCharles3')
 summonerID = me['id']
 puuid = me['puuid']
-API = (f"https://{my_region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summonerID}" + "?api_key=" + api_key)
+APIchampions = (f"https://{my_region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summonerID}" + "?api_key=" + api_key)
 current_champ_list = watcher.data_dragon.champions('12.11.1')
 
-
 def matches(puuid):
+    # Esto hace que busque los datos de mis partidas
     while True:
         my_matches = watcher.match.matchlist_by_puuid(my_region, puuid)
         last_match = my_matches[0]
         while last_match == my_matches[0]:
+            # Mientras que no se juegue partida, que se fija si cambia
             my_matches = watcher.match.matchlist_by_puuid(my_region, puuid)
             time.sleep(1)
-            print(last_match)
-        api(API)
-        print("Ya esta")
+        # Si se jugo una partida, que se llame al metodo api
+        api(APIchampions)
         last_match = my_matches[0]
+
 def bar(dataBase):
+    # Se hace 
     plt.style.use('ggplot')
     plt.bar(dataBase.keys(), dataBase.values(), color='#e36685')
     plt.grid()
@@ -48,29 +50,43 @@ def api(API):
     champsDicPoints = {}
     r = requests.get(API)
     data = json.loads(r.text)
+    # Se consigue de la api los datos necesarios y se pasan a modo json
     for item in data:
         ID = item['championId']
         points = item['championPoints']
         champsDicPoints.update({ID: points})
+        # Se crea un diccionario con los puntos y los ID de los champs
     champsDictName = getAllChampions()
     pointsDictN = sort(champsDicPoints)
     nameDictN = sort(champsDictName)
+    # Se ordenan los diccionarios por el ID, para que esten en sincronia
     for i in nameDictN:
+        # Por cada campeon que hay, que se modifique la Key de todo el diccionario, cambiandola por el nombre del otro diccionario
         try:
             pointsDictN[nameDictN[i]] = pointsDictN[i]
             del pointsDictN[i]
         except:
+            # Si hay un champ que no tiene puntos, que se escriba 0
             pointsDictN[nameDictN[i]] = 0
     toCSV(pointsDictN)
-    bar(pointsDictN)    
+    # Se pasa a .CSV para tenerlo guardado
+    ToJson(pointsDictN)
+    bar(pointsDictN)
+    # Se grafica
+
+def ToJson(data):
+    with open('points.json', "w") as jsonFile:
+        jsonFile.write(json.dumps(data))
 
 def sort(dictionary):
+    # Ordenamiento de diccionarios
     items = dictionary.items()
     itemsSorted = sorted(items)
     newDict = dict((x, y) for x, y in itemsSorted)
     return newDict
 
 def toCSV(dict):
+    # Pasa diccionarios a CSV
     with open('Campeones.csv', 'w') as f:
         writer = DictWriter(f, dict.keys())
         writer.writeheader()
@@ -81,46 +97,13 @@ def getAllChampions():
     file = open('champion.json', encoding= "utf8")
     data2 = json.load(file)
     todo = data2['data']
+    # Se consigue toda la data de los campeones
     for champions in data2['data']:
         champion = todo[champions]
         id = int(champion['key'])
         champsDict.update({id : champions})
+        # Se achica la data para que sea solo el ID y su nombre
     return champsDict
 
-def main(tipo, champ, puntos):
-    print(tipo)
-    print(champ)
-    print(puntos)
-    if int(tipo) == 0:
-        agregar(champ, puntos)
-    elif int(tipo) == 1:
-        eliminar(champ)
-    elif int(tipo) == 2:
-        modificar(champ, puntos)
-
-def agregar(campeon, puntos):
-    with open(r"Campeones.csv", 'a') as df:
-        champ = [campeon, puntos]
-        agregar = writer(df)
-        agregar.writerow(champ)
-        df.close()
-
-def eliminar(campeon):
-    df = pd.read_csv(r"Campeones.csv")
-    posicion = df.Campeones[df.Campeones == campeon].index.tolist()
-    print(posicion)
-    df = df.drop(df.index[posicion])
-    print(df)
-    df.to_csv(r"Campeones.csv", index = False)
-
-def modificar(campeon, puntos):
-    df = pd.read_csv(r"Campeones.csv")
-    posicion = df.Campeones[df.Campeones == campeon].index.tolist()
-    df.loc[posicion] = [campeon, puntos]
-    df['Puntos'] = df['Puntos'].astype(int)
-    df['Puntos'].sort_values()
-    print(df)
-    df.to_csv(r"Campeones.csv", index = False)
-    bar(df['Campeones'], df['Puntos'])
-
-matches(puuid)
+api(APIchampions)
+#matches(puuid)
---
+++ web.py
@@ -0,0 +1,12 @@
+from cProfile import run
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return render_template('table.html')
+
+if __name__ == "__main__":
+    app.run()
